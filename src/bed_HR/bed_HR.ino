77aade18a19bc62ad7b4fc014fe8860eef823df3
Jonathan Hai <haic@mcmaster.ca>
2023-02-05 20:20:08 -0500

    updated HR and MPU code to BED standard

---
+++ src/bed_HR/bed_HR.ino
@@ -0,0 +1,42 @@
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math
+#define HEART_EVENT 20 
+
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library
+#include <Wire.h>
+#include <bed_HR.h>
+
+PulseSensorPlayground pulseSensor;  // Creates an object
+
+void bed_HR_setup() {
+  Serial.begin(19200);
+  
+  // Configure the PulseSensor object, by assigning our variables to it
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED13);       // Blink on-board LED with heartbeat
+  pulseSensor.setThreshold(Threshold);   
+
+  // Double-check the "pulseSensor" object was created and began seeing a signal
+  if (pulseSensor.begin()) {
+    Serial.println("PulseSensor object created");
+  }
+
+  BPM = pulseSensor.getBeatsPerMinute();
+
+}
+
+// This should be triggered whenever you want to measure heartrate. Make sure that the setup is done first.
+void bed_HR_detect() {
+  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis(); // Gets the current time
+  if(currentMillis - HRtime >= HEART_EVENT){
+    BPM = pulseSensor.getBeatsPerMinute();      // Calculates BPM
+    
+    if (pulseSensor.sawStartOfBeat()) {               // Constantly test to see if a beat happened
+      Serial.println("HeartBeat Detected "); // If true, pruint8_t a message
+      Serial.print("BPM: ");
+      Serial.println(BPM);                        // Pruint8_t the BPM value
+      }
+    HRtime = currentMillis;
+  }
+
+}
---
+++ src/libraries/bed_HR.h
@@ -0,0 +1,8 @@
+#ifndef BED_HR_H
+#define BED_HR_H
+unsigned long HRtime = 0;
+const uint8_t PulseWire = 0;       // 'S' Signal pin connected to A0
+const uint8_t LED13 = 13;          // The on-board Arduino LED
+const uint16_t Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore
+uint16_t BPM = 0;
+#endif 
